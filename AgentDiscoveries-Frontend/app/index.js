56ae913e8334cdf6ae2952492dc44e384ae7105b
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-02-05 15:59:20 +0000

    Modify webpack config to include sass and html

--- AgentDiscoveries-Frontend/app/index.js
+++ AgentDiscoveries-Frontend/app/index.js
@@ -1,1 +1,3 @@
+require("./sass/styles.scss"); // Import so that webpack loads all of the sass
+
 console.log("Hello, world...");
--- AgentDiscoveries-Frontend/webpack.config.js
+++ AgentDiscoveries-Frontend/webpack.config.js
@@ -1,7 +1,31 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin("styles.css");
 
 module.exports = {
     entry: './app/index.js',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                fallback: "style-loader"
+            })
+        }]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/static/index.html'
+        }),
+        extractSass
+    ],
 
     output: {
         path: path.join(__dirname, 'target', 'classes', 'META-INF', 'resources', 'public'),
