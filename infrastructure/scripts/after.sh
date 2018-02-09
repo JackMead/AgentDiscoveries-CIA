b85e7e2e5e64614924d98641faba0a2fccd72afa
Anthony Pengelly <Anthony.Pengelly@Softwire.com>
2018-02-09 09:47:51 +0000

    Update to use java 8

--- infrastructure/scripts/after.sh
+++ infrastructure/scripts/after.sh
@@ -3,7 +3,7 @@ set -eux
 
 cd /usr/lib/agentdiscoveries
 
-java -jar agentdiscoveries-backend-1.0-SNAPSHOT.jar &      # You send it in background
+java8 -jar agentdiscoveries-backend-1.0-SNAPSHOT.jar &      # You send it in background
 AGENT_DISCOVERIES_PID=$!                        # You sign it's PID
 echo ${AGENT_DISCOVERIES_PID}                     # You print to terminal
 echo "kill $AGENT_DISCOVERIES_PID" > stop.sh  # Write the the command kill pid in stop.sh
