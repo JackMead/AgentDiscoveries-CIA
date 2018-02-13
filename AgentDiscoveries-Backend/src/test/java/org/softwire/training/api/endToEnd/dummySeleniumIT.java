5bfc5c63eb4088341c5b375036dbdfee2fd55ef5
JackMead <Jack.Mead@softwire.com>
2018-02-13 10:39:36 +0000

    Added ability to pass in target address, prevented front end unit tests running when running end-to-end tests, and updated ReadMe

--- AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/endToEnd/dummySeleniumIT.java
+++ AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/endToEnd/dummySeleniumIT.java
@@ -1,19 +1,18 @@
 package org.softwire.training.api.endToEnd;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.softwire.training.AgentDiscoveriesApplication;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class dummySeleniumIT {
+    public static final String Target_Address=System.getProperty("target.address");
+
     @Test
     public void testTitle(){
         WebDriver driver = new HtmlUnitDriver();
-        driver.get("http://localhost:8080");
+        driver.get(Target_Address);
         String title = driver.getTitle();
         System.out.println("Title found: "+title);
         assertTrue(title.equals("Agent Discoveries"));
@@ -21,7 +20,7 @@ public class dummySeleniumIT {
 
     public void testOnline(){
         WebDriver driver = new HtmlUnitDriver();
-        driver.get("http://localhost:8080");
+        driver.get(Target_Address);
         String body = driver.getPageSource();
         assertTrue(!body.isEmpty());
     }
