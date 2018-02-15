948088937944d09f059cad729ce019eb677d1802
JackMead <Jack.Mead@softwire.com>
2018-02-15 11:15:06 +0000

    Removed profiles page from irrelevant branch

--- AgentDiscoveries-Frontend/app/src/components/app.js
+++ AgentDiscoveries-Frontend/app/src/components/app.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 import Login from "./login";
-import Profile from "./profile";
 import LocationReportSearch from "./search-forms/search-location-reports";
 import RegionSummarySearch from "./search-forms/search-region-summaries";
 import LocationReportSubmit from "./submit-forms/submit-location-report";
@@ -36,9 +35,6 @@ export default class Test extends React.Component {
                                 <RegionSummarySearch />
                             </Tab>
                         </Tabs>
-                    </Tab>
-                    <Tab title="Profile" eventKey={4}>
-                        <Profile />
                     </Tab>
                 </Tabs>
             </div>
