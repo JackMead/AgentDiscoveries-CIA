e94528cad7721b600e62eef516805df5983e5c40
LukaszDygon <ld13281@my.bristol.ac.uk>
2018-02-19 11:28:26 +0000

    messageProcessor java tests

---
+++ AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/core/MessageProcessorTest.java
@@ -0,0 +1,37 @@
+package org.softwire.training.api.core;
+
+import org.junit.jupiter.api.Test;
+import org.softwire.training.models.Message;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class MessageProcessorTest {
+
+    private final MessageProcessor messageProcessor = new MessageProcessor();
+
+    @Test
+    public void EncryptModifiesMessage() {
+        String startingMessageString = "Some test message! :)";
+        Message messageToModify = new Message(startingMessageString);
+        messageProcessor.encrypt(messageToModify);
+        assertNotEquals(startingMessageString, messageToModify.getMessage());
+    }
+
+    @Test
+    public void DecryptModifiesMessage() {
+        String startingMessageString = "Some test message! :)";
+        Message messageToModify = new Message(startingMessageString);
+        messageProcessor.decrypt(messageToModify);
+        assertNotEquals(startingMessageString, messageToModify.getMessage());
+    }
+
+    @Test
+    public void EncryptsAndDecryptsMessageToStartingMessage() {
+        String startingMessageString = "Some test message! :)";
+        Message messageToModify = new Message(startingMessageString);
+        messageProcessor.encrypt(messageToModify);
+        messageProcessor.decrypt(messageToModify);
+        assertEquals(startingMessageString, messageToModify.getMessage());
+    }
+}
