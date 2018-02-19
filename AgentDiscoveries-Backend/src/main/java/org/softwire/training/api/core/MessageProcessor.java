04f3c5219e5f3ad608923e4c269abd21f2c05906
LukaszDygon <ld13281@my.bristol.ac.uk>
2018-02-19 11:11:20 +0000

    message encoded with simple word shift

--- AgentDiscoveries-Backend/src/main/java/org/softwire/training/api/core/MessageProcessor.java
+++ AgentDiscoveries-Backend/src/main/java/org/softwire/training/api/core/MessageProcessor.java
@@ -1,5 +1,43 @@
 package org.softwire.training.api.core;
 
+import org.softwire.training.models.Message;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 public class MessageProcessor {
+    private List<String> wordList = Arrays.asList(
+            "latch", "chief", "dye", "observe", "volleyball", "disturb", "brain", "boys", "giant", "refer", "rigid",
+            "language", "responsible", "strew", "fangs", "sacrifice", "eye", "corrupt", "ambitious", "lead", "hush",
+            "hope", "test", "grandfather", "frequent");
+
+    public void encrypt(Message message){
+        String codeWord = getCodeWord();
+        String messageString = message.getMessage();
+        String encodedMessage = new String();
+        for (int i = 0; i < messageString.length(); i++) {
+            encodedMessage += (char) (Math.floorMod((messageString.charAt(i) + codeWord.charAt(i % codeWord.length())), 128));
+        }
+
+        message.setMessage(encodedMessage);
+    }
+
+    public void decrypt(Message message){
+        String codeWord = getCodeWord();
+        String messageString = message.getMessage();
+        String decodedMessageString = new String();
+        for (int i = 0; i < messageString.length(); i++) {
+            decodedMessageString += (char) (Math.floorMod((messageString.charAt(i) - codeWord.charAt(i % codeWord.length())), 128));
+        }
+
+        message.setMessage(decodedMessageString);
+    }
+
+    private String getCodeWord() {
+        LocalDate today = LocalDate.now();
+        int hashCode = today.hashCode();
+        return wordList.get(hashCode % (wordList.size()));
+    }
 
 }
--- AgentDiscoveries-Backend/src/main/java/org/softwire/training/api/routes/v1/MessageProcessorRoutes.java
+++ AgentDiscoveries-Backend/src/main/java/org/softwire/training/api/routes/v1/MessageProcessorRoutes.java
@@ -1,6 +1,7 @@
 package org.softwire.training.api.routes.v1;
 
 import org.softwire.training.api.core.JsonRequestUtils;
+import org.softwire.training.api.core.MessageProcessor;
 import org.softwire.training.models.Message;
 import spark.Request;
 import spark.Response;
@@ -10,19 +11,25 @@ import javax.inject.Inject;
 public class MessageProcessorRoutes {
     private Request req;
     private Response res;
+    private MessageProcessor messageProcessor;
 
     @Inject
     public MessageProcessorRoutes( ) {
+        this.messageProcessor = new MessageProcessor();
     }
 
     public Message EncryptMessage(Request req, Response res) {
         System.out.print(req.body());
         Message message = JsonRequestUtils.readBodyAsType(req, Message.class);
+        messageProcessor.encrypt(message);
+        System.out.print(message.getMessage());
         return message;
     }
 
     public Message DecryptMessage(Request req, Response res) {
+        System.out.print(req.body());
         Message message = JsonRequestUtils.readBodyAsType(req, Message.class);
+        messageProcessor.decrypt(message);
         return message;
     }
 }
