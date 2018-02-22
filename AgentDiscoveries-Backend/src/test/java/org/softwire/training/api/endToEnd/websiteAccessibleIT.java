34817fa8b591c2f3ed8c4ddbccd074bcd8ae1ddf
JackMead <Jack.Mead@softwire.com>
2018-02-22 09:58:36 +0000

    Broken tests

--- AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/endToEnd/websiteAccessibleIT.java
+++ AgentDiscoveries-Backend/src/test/java/org/softwire/training/api/endToEnd/websiteAccessibleIT.java
@@ -1,27 +1,22 @@
 package org.softwire.training.api.endToEnd;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class dummySeleniumIT {
+public class websiteAccessibleIT {
     public static final String Target_Address=System.getProperty("target.address");
+    public static final WebDriver driver = new HtmlUnitDriver();
 
     @Test
     public void testTitle(){
-        WebDriver driver = new HtmlUnitDriver();
         driver.get(Target_Address);
         String title = driver.getTitle();
         assertTrue(title.equals("Agent Discoveries"));
-    }
-
-    @Test
-    public void testOnline(){
-        WebDriver driver = new HtmlUnitDriver();
-        driver.get(Target_Address);
-        String body = driver.getPageSource();
-        assertTrue(!body.isEmpty());
     }
 }
