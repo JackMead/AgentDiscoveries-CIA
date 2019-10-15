c63d50ad5bcde65bf938712748c22bca4401aabb
zee <zee.saydullaev@aexp.com>
2019-10-15 11:22:32 +0100

    most wanted suspect images

--- AgentDiscoveries-Frontend/app/src/components/nav.jsx
+++ AgentDiscoveries-Frontend/app/src/components/nav.jsx
@@ -86,7 +86,6 @@ export default class NavigationBar extends React.Component {
                 <NavItem componentClass={Link} href='/message' to='/message' eventKey={5}>
                     Today's Message
                 </NavItem>
-{/*                 //TODO */}
                 <NavItem componentClass={Link} href='/mostwanted' to='/mostwanted' eventKey="mostwanted">
                     Most Wanted
                 </NavItem>
